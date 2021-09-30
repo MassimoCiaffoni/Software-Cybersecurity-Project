c54fa6b0f1ee5d0d77a9402c034a888c3b396d46
"MassimoCiaffoni" <"ciaffoni.massimo98@gmail.com">
2021-09-30 16:52:06 +0200

    aggiunta funzioni event.sol

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -10,6 +10,8 @@ contract Event {
     
     struct EventData{ //event data structure
     
+        uint id;
+    
         string title;
         
         string luogo;
@@ -30,26 +32,45 @@ contract Event {
     //all events
     EventData[] private events;
     
+    modifier only_owner() {
+        require(msg.sender == owner, "Non sei autorizzato ad eseguire questa azione");
+        _;
+    }
     
-    event EventCreated(uint indexed eventId, address indexed creator);
     
- 
+    event EventCreated(uint indexed eventid, address indexed creator);
+    event EventOverrlue(uint indexed eventid, address indexed creator);
     
     
-    function  create_event(string memory title, string memory  luogo, string memory  date, uint seats, int price) public {
-        require(msg.sender==owner , "Non sei autorizzato ad effettuare questa azione");{
-            uint id=get_event_length()+1;
-            events.push(EventData(title, luogo,  date, seats, seats, price, "Non concluso", owner));
+    function  create_event(string memory title, string memory  luogo, string memory  date, uint seats, int price) public only_owner {
+        {
+            events.push(EventData(0, title, luogo,  date, seats, seats, price, "Non concluso", owner));
+            uint id=events.length - 1;
+            events[id].id=id;
             emit EventCreated(id, owner);
         }
     }
     
+    function overrlue_event(uint id) public only_owner returns(EventData memory){
+            EventData memory evento=get_event(id);
+            evento.state= "Annullato";
+            emit EventOverrlue(id, owner);
+            return evento;
+    }
     
      function get_events() public view returns (EventData[] memory){
          return events;
      } 
      
-     function get_event_length() public view returns (uint){
+     function get_event(uint id) public view returns(EventData memory evento){
+         require(id < get_event_length(), "L'evento non esiste");
+         for(uint i=0; i < get_event_length(); i++){
+             if(events[i].id==id)
+                return events[i];
+         }
+     }
+     
+     function get_event_length() internal view returns (uint){
          return events.length;
      }
 
--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -14,8 +14,10 @@ contract Ticket {
     
     TicketData[] private tickets;
     
-    function buy_ticket(uint eventid) public payable {
-        require(events[].stato=="Non concluso", "L'evento è già concluso")
-        require(events[eventid].remaining_tickets > 0, "Nessun biglietto disponibile");
+    function buy_ticket(uint event_id) public payable {
+        EventData evento=get_event(event_id);
+        require(evento.state=="Annullato" , "L'evento è stato Annullato");
+        require(evento.state=="Concluso" , "L'evento è già concluso");
+        require(evento.remaining_tickets > 0, "Nessun biglietto disponibile");
     }
 }
